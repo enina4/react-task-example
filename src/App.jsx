d3a0d5dc404c020b6bf5ca1e33f978d4e169892b
enina4 <enina4@umsa.bo>
2023-04-17 14:26:05 -0400

    Eliminando tareas

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,25 @@ function App() {
     setTasks(data);
   }, []);
 
- function createTask(task){
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]);
+  }
 
-  setTasks([...tasks, {
-    title: task.title,
-    id:tasks.length,
-    description: task.description
-  }]);
- }
+  function deleteTask(taskId) {
+    setTasks(tasks.filter(task=> task.id !== taskId));
+  }
 
   return (
     <>
-      <TaskForm createTask={createTask}/>
-      <TaskList tasks={tasks} />
+      <TaskForm createTask={createTask} />
+      <TaskList tasks={tasks} deleteTask={deleteTask} />
     </>
   );
 }
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-function TaskCard({ task }) {
+function TaskCard({ task, deleteTask }) {
   return (
     <div>
       <h1>{task.title}</h1>
       <p>{task.description}</p>
+      <button onClick={() => deleteTask(task.id)}>Delete task</button>
     </div>
   );
 }
