41a29a2cdf096e6b5e30b1f13b68104044a79f17
enina4 <enina4@umsa.bo>
2023-04-17 15:54:31 -0400

    añadiendo estilos con tailwindcss

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -4,7 +4,7 @@ import { TaskContext } from "../context/TaskContext";
 function TaskForm() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const  {createTask}  = useContext(TaskContext);
+  const { createTask } = useContext(TaskContext);
 
   console.log(createTask);
   const handleSubmit = (e) => {
@@ -18,22 +18,27 @@ function TaskForm() {
     setDescription("");
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        placeholder="Escribe tu tarea"
-        onChange={(e) => {
-          setTitle(e.target.value);
-        }}
-        value={title}
-        autoFocus
-      />
-      <textarea
-        placeholder="Escribe la descripciòn de la tarea"
-        onChange={(e) => setDescription(e.target.value)}
-        value={description}
-      ></textarea>
-      <button>Guardar</button>
-    </form>
+    <div className="max-w-md mx-auto">
+      <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+        <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
+        <input
+          placeholder="Escribe tu tarea"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          value={title}
+          autoFocus
+          className="bg-slate-300 p-3 w-full mb-2"
+        />
+        <textarea
+          placeholder="Escribe la descripciòn de la tarea"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+          className="bg-slate-300 p-3 w-full mb-2"
+        ></textarea>
+        <button className="bg-indigo-500 px-3 py-1 text-white">Guardar</button>
+      </form>
+    </div>
   );
 }
 
